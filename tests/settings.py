ba0220d59d37b5213973db1c0c4de69536d39783
Jake Howard <git@theorangeone.net>
2026-02-06 14:54:25 +0000

    Clean up test settings

--- tests/settings.py
+++ tests/settings.py
@@ -10,45 +10,11 @@ IN_TEST = "IN_TEST" in os.environ or (len(sys.argv) > 1 and sys.argv[1] == "test
 ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.messages",
-    "django.contrib.sessions",
-    "django.contrib.staticfiles",
-    "django_tasks_db",
     "django_tasks_db",
     "tests",
 ]
 
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-                "django.template.context_processors.static",
-            ]
-        },
-    },
-]
-
-MIDDLEWARE = [
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-]
-
-STATIC_URL = "/static/"
-
 SECRET_KEY = "abcde12345"
-
-ROOT_URLCONF = "tests.urls"
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
@@ -64,6 +30,12 @@ if "sqlite" in DATABASES["default"]["ENGINE"]:
 
 USE_TZ = True
 
+TASKS = {
+    "default": {
+        "BACKEND": "django_tasks_db.DatabaseBackend",
+        "QUEUES": ["default", "queue-1"],
+    }
+}
+
 if not IN_TEST:
     DEBUG = True
-    TASKS = {"default": {"BACKEND": "django_tasks_db.DatabaseBackend"}}
